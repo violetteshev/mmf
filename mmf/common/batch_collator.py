703b804adb7ede850a76998d5ee9713862930ed6
Amanpreet Singh <asg@fb.com>
2020-07-15 18:18:38 -0700

    [refactor] Release some restrictions on supported dataset types
    Summary:
    - Allows datasets which are not inherited from BaseDataset to be returned from the DatasetBuilder
    - This will allow us to use IterableDataset which is required for some internal dataset
    - Batches can be directly returned now
    - Sampler doesn't need to have set_epoch method
    - Move fast read to VQABuilder as that's the only builder using it currently.
    - If prepare_batch is not implemented, throw a warning instead of error. User needs to implement it themselves now
    - `update_registry_for_model` is optional as well.
    
    Differential Revision: D22563932
    
    fbshipit-source-id: f7ad288b2b6a14efbc1da06beb8d92a1db5a1d7d

--- mmf/common/batch_collator.py
+++ mmf/common/batch_collator.py
@@ -8,8 +8,18 @@ class BatchCollator:
         self._dataset_type = dataset_type
 
     def __call__(self, batch):
-        # Create and return sample list with proper name and type set
-        sample_list = SampleList(batch)
+        # Create and return sample list with proper name
+        # and type set if it is already not a sample list
+        # (case of batched iterators)
+        if (
+            len(batch) == 1
+            and isinstance(batch, list)
+            and isinstance(batch[0], SampleList)
+        ):
+            sample_list = batch[0]
+        elif not isinstance(batch, SampleList):
+            sample_list = SampleList(batch)
+
         sample_list.dataset_name = self._dataset_name
         sample_list.dataset_type = self._dataset_type
         return sample_list
